3ef792718e6eafb7bd642225c8e8b422e0219cd0
DoMinhHieu1997 <hieudm112@gmail.com>
2022-08-25 21:03:50 +0700

    user register 1

--- router/auth.js
+++ router/auth.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const customError = require("../errors/customError");
 
 router.post("/login", (req, res) => {
    res.send("router login");
 });
 router.post("/register", (req, res) => {
-   res.send("router register");
+   if(!req.body.password || req.body.password.length < 6) {
+      return customError(1,"Mật khẩu cần có ít nhất 6 ký tự!");
+   }
+
+   try {
+      
+   } catch (err) {
+      return customError(1,"Tài khoản hoặc mật khẩu không chính xác!");
+   }
 });
 
 module.exports = router;
