5bc01599fb6e6e46af7d69aee16f3aa7f99f9d6f
DoMinhHieu1997 <36026729+DoMinhHieu1997@users.noreply.github.com>
2022-09-11 08:11:16 +0700

    update user update and get user information

---
+++ controllers/hotelCtrl.js
@@ -0,0 +1,15 @@
+const hotelDb = require("../db/hotelDb");
+
+const getUserInforById = async (userID) => {
+   const userInfor = await userDB.findUserById(userID);
+
+   return userInfor;
+}
+
+const updateUser = async (userId,updateInfor) => {
+   const updatedUser = await userDB.updateUserById(userId,updateInfor);
+
+   return updatedUser;
+}
+
+module.exports = { updateUser, getUserInforById};
