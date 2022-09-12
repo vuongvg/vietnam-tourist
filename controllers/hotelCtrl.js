85589993dec383ad8f50a4819e26e5e1522d68de
DoMinhHieu1997 <36026729+DoMinhHieu1997@users.noreply.github.com>
2022-09-12 16:29:45 +0700

    optimize hotel

--- controllers/hotelCtrl.js
+++ controllers/hotelCtrl.js
@@ -1,34 +1,14 @@
 const hotelModel = require("../models/hotelModel");
 const BasePost = require("./BasePost");
 
-class Restaurant extends BasePost {}
-const restaurant = new Restaurant(hotelModel);
+class Hotel extends BasePost {
+    findFamousHotels = async (page = 1, limit = 10) => {
+        return await this._model
+            .find({ isfamous:true })
+            .skip((page - 1) * limit)
+            .limit(limit);
+    };
+}
+const hotel = new Hotel(hotelModel);
 
-module.exports = restaurant;
-// const hotelDB = require("../db/hotelDb");
-
-// const getListHotel = async () => {
-//    const hotelsList = await hotelDB.getListHotel();
-
-//    return hotelsList;
-// }
-
-// const createNewHotel = async (hotelInfor) => {
-//    const newHotel = await hotelDB.createNewHotel(hotelInfor);
-
-//    return newHotel;
-// }
-
-// const updateHotelInfor = async (hotelId, updateInfor) => {
-//    const updatedHotel = await hotelDB.updateHotel(hotelId, updateInfor);
-
-//    return updatedHotel; 
-// }
-
-// const deleteHotelInfor = async (hotelId) => {
-//    const deletedHotel = await hotelDB.deleteHotel(hotelId);
-
-//    return deletedHotel;
-// }
-
-// module.exports = { getListHotel, createNewHotel, updateHotelInfor, deleteHotelInfor };
+module.exports = hotel;
