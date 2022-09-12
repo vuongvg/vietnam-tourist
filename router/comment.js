2657c44b949a259311128d684221edf70d1b585e
vuongcdt <vuongcdt@gmail.com>
2022-09-12 21:04:45 +0700

    merge  master

--- router/comment.js
+++ router/comment.js
@@ -1,6 +1,7 @@
 const BaseRouter = require("./BaseRouter");
 const comment = require("../controllers/commentCtrl");
 const { authMdw } = require("../middlewares/authMdw");
+
 class CommentRouter extends BaseRouter {}
 const router = new CommentRouter({ ...comment, middlewareRole: authMdw });
 
