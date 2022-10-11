d96d2efa9f99e666bb427ce40fb9d5e688642abc
vuongcdt <vuongcdt@gmail.com>
2022-10-11 21:06:59 +0700

    fix header response

--- controllers/searchCtrl.js
+++ controllers/searchCtrl.js
@@ -5,10 +5,13 @@ const LocationModel = require("../models/locationModel");
 const RestaurantModel = require("../models/restaurantModel");
 const TourModel = require("../models/tourModel");
 
-const searchAllPostByKeyword = async (keyword) => {
+const searchAllPostByKeyword = async (keyword, page, limit, sort) => {
    const listModel = [HotelModel, BlogModel, TourModel, RestaurantModel, LocationModel];
    const [...result] = await Promise.all(listModel.map((model) => model.find({ $text: { $search: keyword.replace("_", " ") } }).lean()));
-   return result.flat();
+   return {
+      data: result.flat().filter(({ _id }) => _id).slice((page-1)*limit,page*limit),
+      total: result.flat().length
+   };
 };
 
 module.exports = { searchAllPostByKeyword };
