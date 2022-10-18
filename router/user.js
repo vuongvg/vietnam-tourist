eabcd50f5299e6090af123a36dfd7267016a702c
vuongcdt <vuongcdt@gmail.com>
2022-10-18 23:59:19 +0700

    fix auth

--- router/user.js
+++ router/user.js
@@ -1,25 +1,31 @@
 const express = require("express");
 const userCtrl = require("../controllers/userCtrl");
+const { asyncWrapper } = require("../middlewares/asyncWrapper");
 const { authMdw } = require("../middlewares/authMdw");
 const router = express.Router();
 
-router.get("/:id",(req,res,next)=> authMdw(req,res,next), async(req, res) => {
-   try { 
+router.get("/:id",(req, res, next) => authMdw,asyncWrapper(async (req, res) => {
       const userInfor = await userCtrl.getUserInforById(req.params.id);
       res.status(200).json(userInfor);
-   } catch (err) {
-      res.status(400).send("Getting user information failed");
-   }
-});
+      // try {
+      //    const userInfor = await userCtrl.getUserInforById(req.params.id);
+      //    res.status(200).json(userInfor);
+      // } catch (err) {
+      //    res.status(400).send("Getting user information failed");
+      // }
+   })
+);
 
-router.patch("/update/:id", authMdw, async (req, res) => {
-   try {
+router.patch("/update/:id", authMdw, asyncWrapper(async (req, res) => {
       const updatedUser = await userCtrl.updateUser(req.params.id, req.body);
       res.status(200).json(updatedUser);
-   } catch (err) {
-      res.status(400).send("Updating user information failed")
-   }
-});
+      // try {
+      //    const updatedUser = await userCtrl.updateUser(req.params.id, req.body);
+      //    res.status(200).json(updatedUser);
+      // } catch (err) {
+      //    res.status(400).send("Updating user information failed")
+      // }
+   })
+);
 
 module.exports = router;
-
