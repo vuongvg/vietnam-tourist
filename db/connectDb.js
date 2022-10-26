6589fda2d5678238496fda4cbaf7167abd89ae1e
vuongcdt <vuongcdt@gmail.com>
2022-10-26 22:43:52 +0700

    fix search

--- db/connectDb.js
+++ db/connectDb.js
@@ -3,8 +3,8 @@ const { default: mongoose } = require("mongoose");
 
 exports.connectDb = async (uri) => {
    try {
-      // await mongoose.connect(process.env.MONGODB_URI,{ useNewUrlParser: true, useUnifiedTopology: true })
-      await mongoose.connect(uri,{ useNewUrlParser: true, useUnifiedTopology: true })
+      await mongoose.connect(process.env.MONGODB_URI,{ useNewUrlParser: true, useUnifiedTopology: true })
+      // await mongoose.connect(uri,{ useNewUrlParser: true, useUnifiedTopology: true })
       .then(() => {
          console.log('\nConnection successful');
       });
